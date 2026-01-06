5aec318f8c46f35bed9a0609427b6110b128cded
RitoTorri <cortezfrancisco025gmail.com>
2026-01-06 17:46:07 -0400

    Revert "Se mejoro la arquitectura de carpetas"
    This reverts commit e2f8dd55a29776d2d38b170e88787d176785693d.

--- src/controller/compare.controller.js
+++ src/controller/compare.controller.js
@@ -1,22 +1,30 @@
+const { Router } = require('express');
 const validators = require('../validators/compare.validators');
 const responses = require('../helpers/responses');
 const compareStrings = require('../helpers/compareStrings.helpers');
 
-class CompareController {
-    constructor() { }
 
-    compareStrings(req, res) {
-        try {
-            const { string1, string2 } = req.body;
-            const result = compareStrings(string1, string2);
-            responses.responseSuccess(res, 'Successful Comparison', result);
+const router = Router();
 
-        } catch (error) {
-            console.error(error.message);
-            responses.responseErrorInternal(res, error.message);
-        }
+router.post('/', (req, res) => {
+    try {
+        const { string1, string2 } = req.body;
+
+        if (string1 === undefined || string2 === undefined)
+            return responses.responseError(res, 'Please provide both strings. string1 and string2 are required');
+
+        if(validators.validateLength(string1, string2))
+            return responses.reponseErrorFormat(res, 'Strings cannot be longer than 1000 characters');
+
+        if (validators.isEmpty(string1) || validators.isEmpty(string2))
+            return responses.reponseErrorFormat(res, 'Strings cannot be empty');
+
+        const result = compareStrings(string1, string2);
+        return responses.responseSuccess(res, 'Successful Comparison', result + "%");
+
+    } catch (error) {
+        responses.responseErrorInternal(res, error.message);
     }
-}
+});
 
-
-module.exports = CompareController;
+module.exports = router;
